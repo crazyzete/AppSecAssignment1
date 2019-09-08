6557ce87579e76e9fd9ca5281faa1e83975823ae
Matthew Hibshman <mrh583@nyu.edu>
2019-09-07 18:43:19 -0700

    Adding spell.c stub implementation of dictionary functions

---
+++ spell.c
@@ -0,0 +1,26 @@
+#include "dictionary.h"
+
+/**
+ * Returns true if all words are spelled correctly, false otherwise. Array misspelled is populated with words that are misspelled.
+ */
+int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]) {
+
+	return 0;
+}
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check_word(const char* word, hashmap_t hashtable[]) {
+
+	return false;
+}
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
+
+	return false;
+}
+
